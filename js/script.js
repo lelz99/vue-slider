f696c65ac2ea288e01fe8018746f756f0d32b8c7
lelz99 <emanuelesimonetti99@gmail.com>
2024-01-22 16:38:05 +0100

    fix for next and prev button

--- js/script.js
+++ js/script.js
@@ -5,7 +5,23 @@ const app = createApp({
             pictures,
             currentIndex: 0,
         }
-    }
+    },
+    methods:{
+        goToNext(){
+            if(this.currentIndex < 4){
+                this.currentIndex++
+            } else {
+                this.currentIndex = 0
+            }
+        },
+        goToPrev(){
+            if(this.currentIndex === 0){
+                this.currentIndex = this.pictures.length - 1
+            } else {
+                this.currentIndex--
+            }
+        },
+    },
 });
 app.mount('#root');
 
